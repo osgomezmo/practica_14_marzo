4f93b1dbd9db55028d18460ec5f19f16c2f2c134
osgomezmo <osgomezmo@unal.edu.co>
2026-03-15 15:46:33 -0500

    ejercicio tabla de multiplicar resuelto

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function tabla(numero){
-    for{}
+function tabla(){
+
+    let numero = document.getElementById("numero").value
+
+    document.getElementById("resultado").innerHTML = ""
+
+    for(let i = 1; i <= 10; i++){
+
+        document.getElementById("resultado").innerHTML +=
+        numero + " x " + i + " = " + (numero * i) + "<br>"
+
+    }
+
 }
